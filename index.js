3702a274e618b134b5fe2dc88b1604a6ab042b0c
mingwu114app <matwming114@gmail>
2019-01-28 01:13:40 +1100

    initial commit

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-var express = require('express');
-var STS = require('ali-oss').STS;
-var co = require('co');
-var fs = require('fs');
+var express = require("express");
+var STS = require("ali-oss").STS;
+var co = require("co");
+var fs = require("fs");
 var app = express();
 
-app.get('/', function (req, res) {
-  var conf = JSON.parse(fs.readFileSync('./config.json'));
-  var policy;
-  if (conf.PolicyFile) {
-    policy = fs.readFileSync(conf.PolicyFile).toString('utf-8');
-  }
+app.get("/", function(req, res) {
+ res.set("Access-Control-Allow-Origin", "*");
+ var conf = JSON.parse(fs.readFileSync("./config.json"));
+ var policy;
+ if (conf.PolicyFile) {
+  policy = fs.readFileSync(conf.PolicyFile).toString("utf-8");
+ }
 
-  var client = new STS({
-    accessKeyId: conf.AccessKeyId,
-    accessKeySecret: conf.AccessKeySecret,
+ var client = new STS({
+  accessKeyId: conf.AccessKeyId,
+  accessKeySecret: conf.AccessKeySecret
+ });
+
+ co(function*() {
+  var result = yield client.assumeRole(conf.RoleArn, policy, conf.TokenExpireTime);
+  console.log(result);
+
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-METHOD", "GET");
+  res.json({
+   AccessKeyId: result.credentials.AccessKeyId,
+   AccessKeySecret: result.credentials.AccessKeySecret,
+   SecurityToken: result.credentials.SecurityToken,
+   Expiration: result.credentials.Expiration
   });
-
-  co(function* () {
-    var result = yield client.assumeRole(conf.RoleArn, policy, conf.TokenExpireTime);
-    console.log(result);
-
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-METHOD', 'GET');
-    res.json({
-      AccessKeyId: result.credentials.AccessKeyId,
-      AccessKeySecret: result.credentials.AccessKeySecret,
-      SecurityToken: result.credentials.SecurityToken,
-      Expiration: result.credentials.Expiration
-    });
-  }).then(function () {
-    // pass
-  }).catch(function (err) {
-    console.log(err);
-    res.status(400).json(err.message);
+ })
+  .then(function() {
+   // pass
+  })
+  .catch(function(err) {
+   console.log(err);
+   res.status(400).json(err.message);
   });
 });
 
-app.listen(3000, function () {
-  console.log('App started.');
+app.listen(3001, function() {
+ console.log("App started.");
 });
